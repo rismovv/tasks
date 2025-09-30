4cfdef35f066dc95948c2d7606d2be4a909c3954
Rishi <rismovv@udel.edu>
2025-09-30 09:54:28 -0400

    Complete DoubleHalf component implementation

--- src/bad-components/DoubleHalf.tsx
+++ src/bad-components/DoubleHalf.tsx
@@ -4,38 +4,26 @@ import { Button } from "react-bootstrap";
 export function DoubleHalf(): React.JSX.Element {
     const [dhValue, setDhValue] = useState<number>(10);
 
-    function Doubler(): React.JSX.Element {
-        return (
-            <Button
-                onClick={() => {
-                    setDhValue(2 * dhValue);
-                }}
-            >
-                Double
-            </Button>
-        );
-    }
-
-    function Halver(): React.JSX.Element {
-        return (
-            <Button
-                onClick={() => {
-                    setDhValue(0.5 * dhValue);
-                }}
-            >
-                Halve
-            </Button>
-        );
-    }
-
     return (
         <div>
             <h3>Double Half</h3>
             <div>
                 The current value is: <span>{dhValue}</span>
             </div>
-            <Doubler></Doubler>
-            <Halver></Halver>
+            <Button
+                onClick={() => {
+                    setDhValue((prevValue) => 2 * prevValue);
+                }}
+            >
+                Double
+            </Button>
+            <Button
+                onClick={() => {
+                    setDhValue((prevValue) => 0.5 * prevValue);
+                }}
+            >
+                Halve
+            </Button>
         </div>
     );
 }
